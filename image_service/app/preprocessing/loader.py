ba994dddef2e4e26f6e6780b9afcd10a229faee8
IvanGapon <vangapone40@mail.ru>
2026-07-04 18:22:25 +0300

    обавлена папка service с app.py

--- image_service/app/preprocessing/loader.py
+++ image_service/app/preprocessing/loader.py
@@ -1,6 +1,7 @@
+import tempfile
 from pathlib import Path
 from app.models.image_data import ImageData
-
+import numpy as np
 import cv2
 
 from app.preprocessing.base import ProcessingStep
@@ -56,4 +57,34 @@ class ImageLoader(ProcessingStep):
             height=height,
             channels=channels,
             dtype=str(image.dtype),
+        )
+
+    def from_bytes(self, image_bytes: bytes, filename: str = "image.jpg") -> ImageData:
+        # decode bytes → numpy image
+        np_arr = np.frombuffer(image_bytes, np.uint8)
+        image = cv2.imdecode(np_arr, cv2.IMREAD_UNCHANGED)
+
+        if image is None:
+            raise ValueError("Cannot decode image from bytes")
+
+        # shape parsing
+        if image.ndim == 2:
+            height, width = image.shape
+            channels = 1
+        else:
+            height, width, channels = image.shape
+
+        path = Path(filename)
+
+        return ImageData(
+            original_image=image.copy(),
+            image=image,
+            filename=path.name,
+            path=path,
+            width=width,
+            height=height,
+            channels=channels,
+            dtype=str(image.dtype),
+            microns_per_pixel=None,
+            metadata={}
         )
